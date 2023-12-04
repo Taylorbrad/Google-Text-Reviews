2c6824e567da4ae4be6cb49658bfd13f533c4fd3
zapper125 <125aaron@gmail.com>
2023-12-04 15:15:46 -0700

    Added texting stuff

---
+++ pages/api/sendText.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+export default async function sendText()
+{
+    const accountSid = 'ACc46ab394abd3c3961bbf9415f248b98f';
+    const authToken = '2474df09c2a0b54399947c176b9e3277';
+    const client = require('twilio')(accountSid, authToken);
+
+    client.messages
+        .create({
+            body: 'GUH',
+            from: '+18669539161',
+            to: '+18017062051'
+        })
+        .then(message => console.log(message.sid))
+        .done();
+}
