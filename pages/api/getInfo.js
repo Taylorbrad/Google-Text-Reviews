04a63d7c01b1796fe750c42069da8d0b83a46c6d
Aaron Christiansen <125aaron@gmail.com>
2023-12-04 21:19:36 -0700

    more text stuff

---
+++ pages/api/getInfo.js
@@ -0,0 +1,35 @@
+import {collection, doc, getDoc, getDocs, query, where,} from "firebase/firestore";
+import {db, printFirebaseConfig} from "firebase.config.js"
+
+export default async function getReviewRequest(req, res) {
+
+    const {q_requestID} = req.query
+    const {h_requestID} = req.headers
+
+    const requestID = h_requestID === undefined ? q_requestID : h_requestID
+
+    let request;
+
+    printFirebaseConfig()
+
+    const docRef = doc(db, "Review-Requests", requestID)
+    const docSnapshot = await getDoc(docRef)
+    request = docSnapshot.data()
+
+    if (request === undefined)
+    {
+        res.status(404).json(
+            {
+                errorMessage: "No request found with with ID: " + requestID
+            }
+        )
+    }
+    else
+    {
+        res.status(200).json(
+            {
+                request: request
+            }
+        )
+    }
+}
