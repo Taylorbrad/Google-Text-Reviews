b186318e635834da402027a8e7e7ecbf181f1c2e
Aaron Christiansen <125aaron@gmail.com>
2023-12-04 19:30:32 -0700

    test

--- app/send_sms.js
+++ app/send_sms.js
@@ -3,7 +3,6 @@ const accountSid = 'ACc46ab394abd3c3961bbf9415f248b98f';
 const authToken = '2474df09c2a0b54399947c176b9e3277';
 const client = require('twilio')(accountSid, authToken);
 
-
     client.messages
     .create({
         body: 'GUH',
--- pages/api/sendText.js
+++ pages/api/sendText.js
@@ -1,16 +1,12 @@
-import React from 'react'
-
-    const accountSid = 'ACc46ab394abd3c3961bbf9415f248b98f';
-    const authToken = '2474df09c2a0b54399947c176b9e3277';
-    const client = require('twilio')(accountSid, authToken);
+const accountSid = 'ACc46ab394abd3c3961bbf9415f248b98f';
+const authToken = '2474df09c2a0b54399947c176b9e3277';
+const client = require('twilio')(accountSid, authToken);
 
     client.messages
-        .create({
-            body: 'GUH',
-            from: '+18669539161',
-            to: '+18017062051'
-        })
-        .then(message => console.log(message.sid))
+    .create({
+        body: 'GUH',
+        from: '+18669539161',
+        to: '+18017062051'
+    })
+    .then(message => console.log(message.sid))
 
-
-export default sendText
